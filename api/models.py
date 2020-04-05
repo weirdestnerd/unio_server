6934220906fe42f4752e0a940dd43ec06157d917
Olu D Gbadebo <odgbadebo@gmail.com>
2020-04-05 13:50:10 -0500

    updated models to define unique columns

--- api/models.py
+++ api/models.py
@@ -2,18 +2,19 @@ from djongo import models
 
 
 class Chat(models.Model):
-    timestamp = models.DateTimeField(unique=True)
+    timestamp = models.BigIntegerField(primary_key=True)
     message = models.TextField()
-    isBotMessage = models.BooleanField()
+    isBotMessage = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
 
 
 class User(models.Model):
-    username = models.CharField(max_length=50, default='default_username')
+    username = models.CharField(max_length=50, default='default_username', unique=True)
     chats = models.ArrayField(
-        model_container=Chat
+        model_container=Chat,
+        unique=True
     )
 
     objects = models.DjongoManager()
