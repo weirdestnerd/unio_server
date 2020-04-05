7a606771d7a3bca5009ff41ba1e7f8579d0e4b2a
Olu D Gbadebo <odgbadebo@gmail.com>
2020-04-05 13:55:15 -0500

    setup api routes for chat history and bot response

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:username>/chat_history', views.get_user_chats,
+         name='chats-for-username'),
+    path('<str:username>/bot_response', views.generate_response_to_message,
+         name='response-to-message'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
-# Create your views here.
 def index(request):
+
     return HttpResponse('Api index')
+
+
+def get_user_chats(request, username):
+    response = [
+      {
+        "isBotMessage": True,
+        "message": "Hi"
+      },
+      {
+        "isBotMessage": False,
+        "message": "hello"
+      }
+    ]
+    return JsonResponse(response, safe=False)
+
+
+def generate_response_to_message(request, username):
+    response = {
+      "response": "hello there"
+    }
+    return JsonResponse(response)
